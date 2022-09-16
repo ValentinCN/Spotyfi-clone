3a3a27c7da21c828db3415b04c31febaa6a44610
ValentinCN <valentincamposnualart@gmail.com>
2022-09-15 23:56:45 -0300

    pusheando

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import App from './src/App';
 import { Provider } from 'react-redux';
 import store from "./src/app/store"
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+//asdasd
 root.render(
   <React.StrictMode>
     <Provider store={store}>
